4dc397d3301377afa7770d20819addb4e71c959e
Lin <user@FE03>
2016-09-30 09:29:47 +0800

    lyrics_2

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,8 +3,8 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		
-		String lyrics = "Let it go!Let it go!cannot hold back anymore.";
+
+		String lyrics = "Let her go!Let her go!Can't hold back anymore.";
 		System.out.println(lyrics);
 	}
 
